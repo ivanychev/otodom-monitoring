e636747d08ee906a3ed51aa73d25ede6b16ab962
Sergey Ivanychev <sergeyivanychev@gmail.com>
2022-08-31 23:54:15 +0200

    Fix formatting, add retries

--- otodom/__main__.py
+++ otodom/__main__.py
@@ -3,10 +3,12 @@ from datetime import datetime
 
 import click
 from loguru import logger
+from telegram import Bot
 
 from otodom.filter_parser import parse_flats_for_filter
 from otodom.flat_filter import FlatFilter
-from otodom.report import report_new_flats
+from otodom.models import Flat
+from otodom.report import _send_flat_summary, report_new_flats
 from otodom.storage import (
     dump_fetched_flats,
     dump_new_flats,
@@ -17,7 +19,12 @@ from otodom.storage import (
 )
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.option(
     "--data-path",
     default=".",
@@ -25,7 +32,7 @@ from otodom.storage import (
 )
 @click.option("--bot-token", required=True, help="The Telegram bot token to use.")
 @click.option("--send-report", default=True, help="Send report to the Channel.")
-def main(data_path: str, bot_token: str, send_report: bool):
+def fetch(data_path: str, bot_token: str, send_report: bool):
     logger.info("Hey there!")
     data_path = pathlib.Path(data_path).absolute()
     storage_context = init_storage(data_path)
@@ -49,8 +56,28 @@ def main(data_path: str, bot_token: str, send_report: bool):
     total_flats = get_total_flats_in_db(storage_context.sqlite_conn)
 
     if send_report:
-        report_new_flats(new_flats, total_flats, bot_token, now=ts)
+        report_new_flats(
+            new_flats, total_flats, bot_token, now=ts, report_on_no_new_flats=False
+        )
+
+
+@cli.command()
+@click.option("--bot-token", required=True, help="The Telegram bot token to use.")
+def send_test_flat(bot_token: str):
+    bot = Bot(bot_token)
+    _send_flat_summary(
+        bot,
+        Flat(
+            url="https://www.otodom.pl/pl/oferta/apartament-w-mennica-residence-grzybowska-od-1-09-ID4i622",
+            found_ts=datetime.fromisoformat("2022-08-31T22:54:01.298087"),
+            title="Apartament w Mennica Residence Grzybowska /od 1.09",
+            picture_url="https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6ImR4a29sZzhhMDl6MS1BUEwiLCJ3IjpbeyJmbiI6ImVudmZxcWUxYXk0azEtQVBMIiwicyI6IjE0IiwicCI6IjEwLC0xMCIsImEiOiIwIn1dfQ.80v6yvWASzr4MPicf3zpa6U2Ts0PcoFP4P_y7F2oWjI/image;s=655x491;q=80",
+            summary_location="Mieszkanie na wynajem: Warszawa, \u015ar\u00f3dmie\u015bcie, ul. Grzybowska",
+            price=4500,
+        ),
+        prefix="Test!!!",
+    )
 
 
 if __name__ == "__main__":
-    main()
+    cli()
--- otodom/report.py
+++ otodom/report.py
@@ -1,8 +1,10 @@
 import textwrap
 from datetime import datetime
-from time import sleep
 
+import tenacity
+from loguru import logger
 from telegram import Bot, ParseMode
+from telegram.error import RetryAfter
 from telegram.utils.helpers import escape_markdown
 
 from otodom.models import Flat
@@ -10,38 +12,56 @@ from otodom.models import Flat
 CHANNEL_ID = -1001527642537
 
 
-def _compose_report(flat: Flat):
-    return escape_markdown(
-        textwrap.dedent(
-            f"""\
-    {flat.title}
+def _compose_html_report(flat: Flat, prefix: str):
+    report = textwrap.dedent(
+        f"""\
+    <strong>{flat.title}</strong>
     
-    Location: {flat.summary_location}
-    Price: {flat.price}
-    [Link]({flat.url})
+    <strong>Location:</strong> {flat.summary_location}
+    <strong>Price:</strong>  {flat.price}
+    
+    <a href="{flat.url}">Link</a>
     """
-        ),
-        version=2,
     )
+
+    if prefix:
+        report = f"{prefix}\n{report}"
+    return report
+
+
+@tenacity.retry(
+    wait=tenacity.wait_exponential(min=15, max=60) + tenacity.wait_random(3, 5),
+    stop=tenacity.stop_after_attempt(10),
+    retry=tenacity.retry_if_exception_type((RetryAfter,)),
+)
+def _send_flat_summary(bot: Bot, flat: Flat, prefix: str = ""):
+    bot.send_message(
+        CHANNEL_ID,
+        _compose_html_report(flat, prefix=prefix),
+        parse_mode=ParseMode.HTML,
+        timeout=1000,
+    )
+    if flat.picture_url:
+        bot.send_photo(CHANNEL_ID, flat.picture_url, timeout=1000)
 
 
 def report_new_flats(
-    flats: list[Flat], total_flats: int, bot_token: str, now: datetime
+    flats: list[Flat],
+    total_flats: int,
+    bot_token: str,
+    now: datetime,
+    report_on_no_new_flats: bool,
 ):
     bot = Bot(token=bot_token)
-    bot.send_message(
-        CHANNEL_ID,
-        f"Found {len(flats)} new flats at {now.isoformat()}, total flats: {total_flats}",
+    summary_report = (
+        f"Found {len(flats)} new flats at {now.isoformat()}, total flats: {total_flats}"
     )
-    for flat in flats:
+    logger.info(summary_report)
+
+    if flats or report_on_no_new_flats:
         bot.send_message(
             CHANNEL_ID,
-            _compose_report(flat),
-            parse_mode=ParseMode.MARKDOWN_V2,
-            timeout=1000,
+            summary_report,
         )
-        if flat.picture_url:
-            bot.send_photo(CHANNEL_ID, flat.picture_url, timeout=1000)
-
-        # This is needed in order to throttle the messages stream.
-        sleep(5)
+    for flat in flats:
+        _send_flat_summary(bot, flat)
